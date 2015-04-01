c470ce0afc2ac8f23a143319981a0cf43207b9c5
baicaicai <chaojie.cai@hotmail.com>
2015-04-01 17:56:06 +0800

    Update the query method
    update the query method to obtain the missions from CIA from -7 days
    --Now -- 30 days

--- app/controllers/api.server.controller.js
+++ app/controllers/api.server.controller.js
@@ -8,27 +8,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var _ = require('lodash');
+var moment = require('moment');
 var avServ = require('../services/avcloudServ');
 
 
 //ToDO 将此类机密信息作为常量存储与别处
 var mainPageUrl = 'http://cia.airchina.com.cn/cia/loginHandler.do';
-var contentUrl = 'http://cia.airchina.com.cn/cia/notificationAcknowledge.do';
+var contentUrl 	= 'http://cia.airchina.com.cn/cia/notificationAcknowledge.do';
+var queryUrl 	= 'http://cia.airchina.com.cn/cia/rosterReport.do';
 var dheaders = {
-	'Host': 'cia.airchina.com.cn',
-	'Connection': 'keep-alive',
-	'Content-Length': 0,
-	'Cache-Control': 'max-age=0',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-	'Origin': 'http://cia.airchina.com.cn',
+	'Accept': '*/*',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'zh-CN,zh;q=0.8',
 	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2305.2 Safari/537.36',
 	'Content-Type': 'application/x-www-form-urlencoded',
 	'DNT': '1',
-	'Referer': 'http://cia.airchina.com.cn/cia/notification.page',
-	'Accept-Encoding': 'gzip, deflate',
-	'Accept-Language': 'zh-CN,zh;q=0.8',
-	'Cookie': ''
-};
+	'Cookie': ''};
+
+
+
 var userID = '0000058908';
 var passWord = '0503';
 var flightKey = [
@@ -55,10 +53,17 @@ var flightKey = [
 ];
 
 
+
+
 exports.parseCIA = function () {
 
 //定义cookie变量，之后会将第一轮登陆获取的cookie存入此变量	
 	var cookies;
+
+//定义提交POST请求的时间变量，默认定义刷新任务时间为当前日期的前七天及后一个月
+	var rqstFrom = moment().subtract(7, 'days').format('DDMMMYY');
+	var rqstTo = moment().add(30, 'days').format('DDMMMYY');
+
 //使用request发起第一次post登陆请求，登陆网址为mainPageUrl	
 	request.post(
 		{
@@ -76,14 +81,19 @@ exports.parseCIA = function () {
 			cookies = c2.concat(";", c1, ";");
 			/*			console.log(typeof(cookies));*/
 			dheaders.Cookie = cookies;
-			/*			console.log(dheaders);*/
+/*			console.log(dheaders);
+			console.log(rqstFrom + rqstTo);*/
 			request.post(
 				{
 					url: contentUrl,
-					headers: dheaders
+					headers: dheaders,
+					form:{
+						rqstFrom:rqstFrom,
+						rqstTo:rqstTo
+					}
 				},
 				function (err, response, body) {
-					console.log('成功获取HTML数据');
+					console.log("成功获取从" + rqstFrom + "到" + rqstTo + "的CIA任务数据");
 					parseRosterReport(body);
 				}
 			);
@@ -130,7 +140,7 @@ var parseRosterReport = function (html) {
 		 4.使用字符串功能 .split('|')将字符串拆分成array
 		 */
 		var flightDetail = rawFlighs.eq(i).text().replace(/\n/g, "|").replace(/\s/g, "-").split('|');
-
+/*		console.log(flightDetail);*/
 		//使用underscore的.compact函数删除所有空数据
 		/*	使用underscore的_.map功能来遍历从上面获取的数组的每一个元素，并将每一个元素拼装成
 		 JSON字符串，其中有回调功能有三个参数
@@ -153,6 +163,7 @@ var parseRosterReport = function (html) {
 							// console.log('更新第' + i + '条记录的日期信息为' + lastDate);
 						}
 						else {
+							elem = moment(elem).toDate();
 							lastDate = elem;
 							dateArray.push(elem);
 						}
@@ -183,6 +194,7 @@ var parseRosterReport = function (html) {
 							lastFlightNum = elem;
 						}
 						break;
+
 					//Key是Sector
 					case 3:
 						var tempSector = elem.split('-');
@@ -193,69 +205,14 @@ var parseRosterReport = function (html) {
 				return elem;
 			})
 			.value();
-		Mid = rawFlightArray[0] + rawFlightArray[2] + rawFlightArray[5];
+		//初始化"Mid"字段作为任务识别ID	
+		Mid = moment(rawFlightArray[0]).format('YYYYMMDD') + "_" + rawFlightArray[2]+ "_" +rawFlightArray[5];
 		rawFlightArray.push(Departure, Destination);
 		rawFlightArray.unshift(Uid,Mid);
-		var flightDetail = _.zipObject(flightKey,rawFlightArray)
-		avServ.updateFlights(flightDetail);
+		var flightObject = _.zipObject(flightKey,rawFlightArray);
+		console.log(flightObject.DutyDate);
+		avServ.updateFlights(flightObject);
 
 	};
 
 };
-
-
-/*
- //必填字段，如果为空，则取上一个有值数据 lastDate
- //本子段的key是: Date
-
- case 0:
- if(elem == "-"){
- elem = lastDate;
- // console.log('更新第' + i + '条记录的日期信息为' + lastDate);
- }
- else{
- lastDate = elem;
- }
- elem = "{\"Date\":\"" + elem + "\",";
- break;
-
- //必填字段，如果为空，则取上一个有值数据 lastDay
- //本子段的key是：Day
-
- case 1:
- if(elem == "-"){
- elem = lastDay;
- // console.log('更新第' + i + '条记录的星期为' + lastDay);
- }
- else{
- lastDay = elem;
- }
- elem = "\"Day\":\"" + elem + "\",";
- break;
-
- //如果为空且任务类型为"FLY"或者"LO"则取上一个有值数据 lastFlightNum
- //本子段的key是：FlightNum
-
- case 2:
- if(elem == "-" && (array[5]==="FLY"|| array[5] ==="LO")){
- elem = lastFlightNum;
- // console.log('更新第' + i + '条记录的航班编号为' + lastFlightNum);
- }
- else{
- lastFlightNum = elem;
- }
- elem = "\"FlightNum\":\"" + elem + "\",";
- break;
- //Key是Sector
- case 3:
- var tempSector = elem.split('-');
- Departure = "\"Departure\":\"" + tempSector[0] + "\",";
- Destination = "\"Destination\":\"" + tempSector[1] + "\"}";
- elem = "\"Sector\":\"" + elem + "\",";
- break;
-
- }
- return elem;
- })
- .value();
- */
--- app/services/avcloudServ.js
+++ app/services/avcloudServ.js
@@ -10,23 +10,36 @@ var AV = require('avoscloud-sdk').AV;
 var _ = require('lodash');
 
 
+
+
+
 exports.updateFlights = function(data){
 		//初始化AVcloud链接
 		AV.initialize(config.avoscloudAppID, config.avoscloudAppKey);
+
+		//初始化Flight document
 		var Flight = AV.Object.extend('Flight');
+
+		var CurrentMisson =  AV.Query(Flight).then(function(){
+						
+						console.log(typeof(CurrentMisson));
+						console.log(CurrentMisson);
+
+		});
+
+
 		var flight = new Flight();
 		_.map(data, function (elem, key, list) {
 			flight.set(key, elem);
 		});
+		
 		flight.save(null,{
 			success: function(flight) {
 				// Execute any logic that should take place after the object is saved.
-				console.log('New object created with objectId: ' + flight.Mid);
 			},
 			error: function(flight, error) {
 				// Execute any logic that should take place if the save fails.
 				// error is a AV.Error with an error code and description.
-				console.log('Failed to create new object, with error code: ' + error.description);
 			}
 		});
 	};
